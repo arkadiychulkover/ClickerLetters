80dbc278ad061bd84fd49a3aecf0509e18b73538
Arch <arkadii@gmail.com>
2025-11-29 14:55:26 +0200

    Work with Ui

--- js/GameController.js
+++ js/GameController.js
@@ -6,10 +6,23 @@ import {
     GetMoney, 
     GetExpCof, 
     GetMoneyCof, 
-    GetTimeRange 
+    GetTimeRange ,
+    GetExpLvl,
+    ChangeExpLvl,
+    GetIndexLocation,
+    GetIndexMus,
+    ChangeMusic,
+    ChangeLoc
 } from "./DictController.js";
 
-import { GetLevelOfUpgrade } from "./Ui.js";
+import { 
+    GetLevelOfUpgrade,
+    ChangeBackgroundMusic,
+    ChangeBacgroundImg,
+    ChangeLevelOfVacabuular,
+    ChangeShkalaOfVacabular,
+    ChangeAmountOfValute
+} from "./Ui.js";
 
 let curentKeys = []; 
 let currentLettersDivs = [];
@@ -38,6 +51,7 @@ document.addEventListener('keydown', async (e) =>
     let moneyCof = GetMoneyCof();
     let expCof = GetExpCof();
     let money = GetMoney();
+    let expLvl = GetExpLvl();
 
     let levelOfMoneyUpgrade = GetLevelOfUpgrade("moneyUpgrade");
     let levelOfExpUpgrade = GetLevelOfUpgrade("expUpgrade");
@@ -45,7 +59,28 @@ document.addEventListener('keydown', async (e) =>
     let moneyToAdd = Math.floor(1 * moneyCof * levelOfMoneyUpgrade);
     let expToAdd = Math.floor(1 * expCof * levelOfExpUpgrade);
 
+    let newLvlExp = expLvl + expToAdd;
+
     ChangeMoney(money + moneyToAdd);
+    ChangeExpLvl(expLvl + expToAdd);
+
+    ChangeAmountOfValute(money + moneyToAdd);
+
+    if (Math.floor(expLvl/100) < Math.floor((expLvl + expToAdd)/100))
+    {
+        let indexLocation = GetIndexLocation();
+        let indexMusic = GetIndexMus(); 
+
+        ChangeLoc((indexLocation + 1));
+        ChangeMusic((indexMusic + 1));
+
+        ChangeBackgroundMusic((indexMusic + 1));
+        ChangeBacgroundImg((indexLocation + 1));
+
+        let percent = newLvlExp - Math.floor(newLvlExp / 100) * 100;
+        ChangeLevelOfVacabuular(Math.floor(newLvlExp / 100));
+        ChangeShkalaOfVacabular(percent);
+    }
 
     console.log(`You earned ${moneyToAdd} money and ${expToAdd} exp!`);
 });
