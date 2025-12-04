b9bb6067cac1fc3f189a9f0752a9296415da7c69
kladkovitalik@gmail.com <kladkovitalik@gmail.com>
2025-12-04 20:25:20 +0000

    unpacking 1

---
+++ PHP/unpacking.php
@@ -0,0 +1,14 @@
+<?php
+$sourcePath = __DIR__ ../ '/1.zip'; 
+$targetFolder = __DIR__ ../'/unpacking files'; 
+if (!is_dir($targetFolder)) {
+    mkdir($targetFolder, 0777, true);
+}
+$fileName = basename($sourcePath);
+$targetPath = $targetFolder . DIRECTORY_SEPARATOR . $fileName;
+if (rename($sourcePath, $targetPath)) {
+    echo "Файл успешно перемещён в $targetPath";
+} else {
+    echo "Ошибка при перемещении файла";
+}
+?>
